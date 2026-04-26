e78fa65a69a567e5582723da1d1fd807e9425671
Septian Dika <83001459+sed1ka@users.noreply.github.com>
2026-04-27 00:31:07 +0700

    add a static rendermode

--- lib/modes/render_mode.dart
+++ lib/modes/render_mode.dart
@@ -2,11 +2,17 @@ import 'package:flutter/widgets.dart';
 import 'package:formunda/models/formunda_node.dart';
 
 part 'column_mode.dart';
+
 part 'list_view_mode.dart';
+
 part 'sliver_list_mode.dart';
 
 abstract class RenderMode {
   const RenderMode();
+
+  static const RenderMode column = ColumnMode();
+  static const RenderMode listView = ListViewMode();
+  static const RenderMode sliverList = SliverListMode();
 
   Widget buildBodyWidget(
     BuildContext context,
